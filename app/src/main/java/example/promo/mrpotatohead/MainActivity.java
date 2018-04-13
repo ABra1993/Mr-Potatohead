75d8c52063909ac1ea6072b91f8ea4b636fd5271
ABra1993 <abrands1993@gmail.com>
2018-04-13 15:40:56 +0200

    Version1.2

--- app/src/main/java/example/promo/mrpotatohead/MainActivity.java
+++ app/src/main/java/example/promo/mrpotatohead/MainActivity.java
@@ -1,3 +1,15 @@
+/**
+ * File: MainActivity.java
+ *
+ * Author: Amber Brands
+ *
+ * Date: 13-4-2018
+ *
+ * Description: This script is part of the application Mr. Potatohead. Initially it display the
+ * toy’s body. This script checks if the user checks/unchecks any of the checkboxes below the toy,
+ * and makes the corresponding body part or accessory visible/invisible.
+ */
+
 package example.promo.mrpotatohead;
 
 import android.media.Image;
@@ -11,12 +23,20 @@ import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * Integer array containing ID's of items.
+     */
     protected int[] imageID = new int[]{R.id.arms, R.id.ears, R.id.eyebrows, R.id.eyes,
             R.id.glasses, R.id.hat, R.id.mouth, R.id.mustache, R.id.nose, R.id.shoes};
 
+    /**
+     * String array containing names of items.
+     */
     protected String[] items = new String[]{"arms", "ears", "eyebrows", "eyes", "glasses", "hat",
             "mouth", "mustache", "nose", "shoes"};
-
+    /**
+     * Boolean array containing visibility of items.
+     */
     protected Boolean[] itemVisible = new Boolean[10];
 
     @Override
@@ -26,12 +46,17 @@ public class MainActivity extends AppCompatActivity {
 
         if (savedInstanceState != null){
 
+            /**
+             * Loops through different items and checks visibility.
+             */
             for (int i = 0; i < 10; i++){
                 itemVisible[i] = savedInstanceState.getBoolean(items[i], false);
             }
 
+            /**
+             * Makes items visible if respective item was visible in previous activity.
+             */
             ImageView image;
-
             for (int i = 0; i < 10; i++){
                 if (itemVisible[i]){
                     image = findViewById(imageID[i]);
@@ -45,8 +70,10 @@ public class MainActivity extends AppCompatActivity {
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState); // always call super
 
+        /**
+         * Checks and stores whether items are visible in current state.
+         */
         ImageView image;
-
         for (int i = 0; i < 10; i++){
             image = findViewById(imageID[i]);
             if (image.getVisibility() == View.VISIBLE){
@@ -57,12 +84,13 @@ public class MainActivity extends AppCompatActivity {
 
     public void checkClicked(View view) {
         Log.d("potato", "checkClicked: ");
-
         android.widget.CheckBox checkbox = (android.widget.CheckBox) view;
         String item = checkbox.getText().toString();
 
+        /**
+         * Checks checkbox and finds corresponding ImageView.
+         */
         ImageView image;
-
         if (Objects.equals(item, "Arms")) {
             image = findViewById(R.id.arms);
         } else if (Objects.equals(item, "Ears")) {
@@ -85,6 +113,9 @@ public class MainActivity extends AppCompatActivity {
             image = findViewById(R.id.shoes);
         }
 
+        /**
+         * Sets image to visible/invisible depending on current state.
+         */
         if (image.getVisibility() == View.INVISIBLE) {
             image.setVisibility(View.VISIBLE);
         } else {
